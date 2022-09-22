b09a4cbab5d648c2c56def1e56b0cf57ace8fe29
My <viquynhmy.n@gmail.com>
2022-09-22 14:55:41 +1000

    Errors fixed re. Tweepy updates

--- tweet_obtain/tweet_obtain.py
+++ tweet_obtain/tweet_obtain.py
@@ -31,14 +31,14 @@ for tweet in tweets_copy:
         text = api.get_status(id=tweet.id, tweet_mode='extended').full_text
     except:
         pass
-    tweets_df = tweets_df.append(pd.DataFrame({'user_name': tweet.user.name, 
+    tweets_df = pd.concat([tweets_df,pd.DataFrame({'user_name': tweet.user.name, 
                                                'user_location': tweet.user.location,\
                                                'user_description': tweet.user.description,
                                                'user_verified': tweet.user.verified,
                                                'date': tweet.created_at,
                                                'text': text, 
                                                'hashtags': [hashtags if hashtags else None],
-                                               'source': tweet.source}))
+                                               'source': tweet.source})])
     tweets_df = tweets_df.reset_index(drop=True)
 
 # show the dataframe
