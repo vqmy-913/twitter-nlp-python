22bad54457ad9447d137366e5b208bcf8c116f2d
Chitrakshi <chitrakshigosain@gmail.com>
2022-09-25 17:31:11 +1000

    adding sentimental_analysis function to to fetch tweets and start their sentimental analysis

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -4,16 +4,18 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
 from operator import itemgetter
+import tweet_obtain
 
 # References: 
 # - https://cloud.ibm.com/apidocs/natural-language-understanding?code=python#features-examples
 # - https://towardsdatascience.com/how-to-use-the-ibm-watson-tone-analyzer-to-perform-emotional-sentiment-analysis-in-python-d1d4ad022d0b
 # - https://github.com/IBM/use-advanced-nlp-and-tone-analyser-to-analyse-speaker-insights
+# - https://betterprogramming.pub/using-python-pandas-with-excel-d5082102ca27
 
 natural_language_understanding = ""
 
 """
-This function authenticates developer credentials with Watson Natural Language Understanding API and
+Authenticates developer credentials with Watson Natural Language Understanding API and
 establishes a secure connection for developemnt
 """
 def NLU_authentication() -> None:
@@ -86,3 +88,30 @@ def analyse_keywords(keywords: list) -> list:
         keywords_sentiments_emotions.append(keywords_sentiments_emotions_buffer)
 
         return keywords_sentiments_emotions
+
+"""
+Performs the sentimental analysis on the tweets obtained for the keyword enetered by user.
+
+Begins by establishing a secure connection, followed by retrieving the tweets from tweet_obtain.py,
+loops through all the tweets by quarter, performs a sentimental analysis on each of them, and stores
+the sentiment associated with the tweet in a nested list along with the quarter of the year that
+tweet belongs to,this is followed by converting the data in nested list to a dataframe and finally
+ends with writing the data of the dataframe to an enxcel sheet which will then be outputted on the
+dashboard using Power BI
+"""
+def sentimental_analysis():
+    NLU_authentication()
+
+    tweets_dict = tweet_obtain.fetchTweets()
+
+    tweet_sentiment = []
+    for quarter in tweets_dict:
+        for tweet in tweets_dict[quarter]:
+            tweet_sentiment.append([quarter, tweet, analysing_sentiments(tweet)[0].get('sentiment')])
+    
+    sentiment_df = pd.DataFrame(tweet_sentiment, columns=['Quarter','Tweet','Sentiment'])
+    
+    sentiment_df.to_excel("Sentimental Analysis of Tweets.xlsx", index=False, sheet_name="sheet1")
+
+# Calling the function to fetch tweets and start their sentimental analysis
+sentimental_analysis()
